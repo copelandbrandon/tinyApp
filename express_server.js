d5261296bd8950d640cd5472e71931253089c7a2
Brandon Copeland <copelandbrandon2@gmail.com>
2021-05-10 20:34:11 +0000

    added a view template for shortening a new url and installed bodyparser as a dependency

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -21,24 +23,20 @@ app.get("/hello", (req, res)=>{
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
-
 app.get("/urls", (req, res)=>{
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
  });
+
+app.get("/urls/new", (req, res)=>{
+  res.render("urls_new");
+});
  
 app.get("/urls/:shortURL", (req, res)=>{
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
