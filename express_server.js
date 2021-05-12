d2ba235afe45c46f466581c7cc1f47637b011b43
Brandon Copeland <copelandbrandon2@gmail.com>
2021-05-12 20:53:41 +0000

    implemented registration page and handler including error cases and created a new login view

--- express_server.js
+++ express_server.js
@@ -9,6 +9,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(cookieParser());
 
+const users = {
+  "randomUserID": { 
+    id: "randomUserID",
+    email: "user@example.com",
+    password: "examplePassword"
+  },
+  "randomUserID2": {
+    id:"randomUserID2",
+    email:"user2@example.com",
+    password: "examplePassword2"
+  }
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -18,6 +31,18 @@ const generateRandomString = function() {
   return Math.random().toString(36).substring(7);
 };
 
+//returns true if email is found otherwise returns false
+const findEmail = function(userEmail) {
+  for (let user in users) {
+    console.log("user ID: ",users[user].email);
+    if (users[user].email === userEmail) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//home page route
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -25,20 +50,41 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res)=>{
   res.json(urlDatabase);
 });
-
+//hello route
 app.get("/hello", (req, res)=>{
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+//registration page endpoint
+app.get("/register", (req, res)=>{
+  const templateVars = {user: users[req.cookies["user_id"]]};
+  res.render("urls_registration", templateVars)
+});
+
+// registration submit handler
+app.post("/register", (req, res)=>{
+  const randomID = generateRandomString();
+  if (req.body.email === "" || req.body.password === "" || findEmail(req.body.email) === true) {
+    res.sendStatus(400);
+  }
+  users[randomID] = { id: randomID, email: req.body.email, password: req.body.password}
+  res.cookie("user_id", randomID);
+  res.redirect("/urls")
+  console.log(users);
+});
+
+//urls list endpoint
 app.get("/urls", (req, res)=>{
-  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
+  const templateVars = {urls: urlDatabase, user: users[req.cookies["user_id"]]};
   res.render("urls_index", templateVars);
  });
 
+//add urls endpoint
 app.get("/urls/new", (req, res)=>{
-  const templateVars = {username: req.cookies["username"]};
+  const templateVars = {user: users[req.cookies["user_id"]]};
   res.render("urls_new", templateVars);
 });
+
 //create new short url
 app.post("/urls", (req, res)=>{
   let shortURL = generateRandomString();
@@ -46,40 +92,45 @@ app.post("/urls", (req, res)=>{
   urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`);
 });
+
 //delete existing url
 app.post("/urls/:shortURL/delete", (req, res)=>{
   let shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect("/urls");
 });
+
 //logout route
 app.post("/logout", (req,res)=>{
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
+
 //updates existing
 app.post("/urls/:shortURL", (req, res)=>{
-  let shortURL = req.params.shortURL;
-  let newLong = req.body.longURL;
+  const shortURL = req.params.shortURL;
+  const newLong = req.body.longURL;
   urlDatabase[shortURL] = newLong;
   res.redirect(`/urls/${shortURL}`)
 });
+
 //login route
-app.post("/login", (req,res)=>{
-  let cookie = req.body.username;
-  res.cookie("username", cookie);
-  res.redirect("/urls")
+app.get("/login", (req,res)=>{
+  const templateVars = {user: users[req.cookies["user_id"]]};
+  res.render("/urls_login", templateVars);
 });
 
+// redirect for shortURL's
 app.get("/u/:shortURL", (req, res)=>{
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
- 
-app.get("/urls/:shortURL", (req, res)=>{
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
-  res.render("urls_show", templateVars);
-});
+
+ //individual url page endpoint
+// app.get("/urls/:shortURL", (req, res)=>{
+//   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
+//   res.render("urls_show", templateVars);
+// });
 
 
 app.listen(PORT, () => {
